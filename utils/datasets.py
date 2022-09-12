36a7cbb1686ca1d2167d3ba08bc05849ac6ed790
VietDunghacker <trungdunghoang92@gmail.com>
2022-09-12 19:25:44 +0900

    Add cutout

--- utils/datasets.py
+++ utils/datasets.py
@@ -584,9 +584,8 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
             if min([hyp['hsv_h'], hyp['hsv_s'], hyp['hsv_v']]) > 0:
                 augment_hsv(img, hgain=hyp['hsv_h'], sgain=hyp['hsv_s'], vgain=hyp['hsv_v'])
 
-            # Apply cutouts
-            # if random.random() < 0.9:
-            #     labels = cutout(img, labels)
+            if random.random() < 0.9:
+                labels = cutout(img, labels)
             
             if random.random() < hyp['paste_in']:
                 sample_labels, sample_images, sample_masks = [], [], [] 
