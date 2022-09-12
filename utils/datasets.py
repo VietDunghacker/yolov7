1198922fecff4310cd49563ed37ccdb1a4e58e09
VietDunghacker <trungdunghoang92@gmail.com>
2022-09-12 21:49:57 +0900

    Add focus bounding box

--- utils/datasets.py
+++ utils/datasets.py
@@ -603,7 +603,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
             img, labels = random_mask_face(self, img, labels, hyp['mask_face'])
             img, labels = focus_bounding_box(self, img, labels)
-            
+
             img, labels = self.albumentations(img, labels)
 
         nL = len(labels)  # number of labels
@@ -1212,7 +1212,7 @@ class Albumentations:
         import albumentations as A
 
         self.transform = A.Compose([
-            A.Cutout(num_holes=25, max_h_size=24, max_w_size=24, fill_value=0, p=1.0),
+            A.Cutout(num_holes=30, max_h_size=10, max_w_size=10, fill_value=0, p=1.0),
             A.ShiftScaleRotate(shift_limit=0, scale_limit=0.1, rotate_limit=1, interpolation=1, border_mode=0),
             A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
             A.RGBShift(r_shift_limit=20, g_shift_limit=20, b_shift_limit=20),
